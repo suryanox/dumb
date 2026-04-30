add377d65a1413c8fb350cbe7d47efede2ceb8c3
Sooraj <sooraj.crafts@gmail.com>
2026-04-30 15:33:37 +0800

    Create blocking.rs

---
+++ futures/blocking.rs
@@ -0,0 +1,24 @@
+use tokio::time::{sleep, Duration};
+use std::thread;
+
+async fn async_task(id: u8) {
+    for i in 0..5 {
+        println!("task {id} tick {i}");
+        sleep(Duration::from_millis(200)).await;
+    }
+}
+
+async fn blocking_task() {
+    println!("blocking task started");
+    thread::sleep(Duration::from_secs(5));
+    println!("blocking task finished");
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::join!(
+        async_task(1),
+        async_task(2),
+        blocking_task(),
+    );
+}
